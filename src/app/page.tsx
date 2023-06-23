749e1cd05af8a9522ff4e409a960fc1c3f4c8f43
Olya <polyanskaya3000@gmail.com>
2023-06-23 15:34:11 +0700

    feat: add project skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.css';
 
-export default function Home() {
+
+export default function Home(): JSX.Element {
+  const a = 256;
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -87,9 +89,10 @@ export default function Home() {
           </h2>
           <p>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
+            YOUR DATA {a}
           </p>
         </a>
       </div>
     </main>
-  )
+  );
 }
